6e2b7f509c4a6dee2a7ec2202796e4c64024e9c4
Parkrister <sergey.12sed@gmail.com>
2020-10-26 16:42:24 +0300

    Class Stack is added

--- ConsoleApplication1/Stack.h
+++ ConsoleApplication1/Stack.h
@@ -2,5 +2,84 @@
 template <class T>
 class Stack
 {
+	int size, MaxSize;
+	T* mas;
+public:
+	Stack (int _MaxSize)
+	{
+		MaxSize = _MaxSize;
+		mas = new T[MaxSize];
+		size = 0;
+	}
+
+	Stack (const Stack <T>& m) //конструктор копирования
+	{
+		MaxSize = m.MaxSize;
+		mas = new T[MaxSize];
+		size = m.size;
+		for (int i = 0; i < size; i++)
+		{
+			mas[i] = m.mas[i];
+		}
+	}
+
+	~Stack() //деструктор
+	{
+		delete[]mas;
+	}
+
+	Stack& operator=(const Stack <T>& m)
+	{
+		if (MaxSize != m.MaxSize)
+		{
+			delete[]mas;
+			MaxSize = m.MaxSize;
+			mas = new T[MaxSize];
+		}
+		size = m.size;
+		for (int i = 0; i < size; i++)
+		{
+			mas[i] = m.mas[i];
+		}
+		return *this;
+	}
+
+	bool Empty()
+	{
+		if (size == 0)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	bool Full()
+	{
+		if (size == MaxSize)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	void Push(T a)
+	{
+		if (Full())
+		{
+			throw MaxSize;
+		}
+		mas[size] = a;
+		size++;
+	}
+
+	T Pop()
+	{
+		if (Empty())
+		{
+			throw 0;
+		}
+		size--;
+		return mas[size];
+	}
 
 };
